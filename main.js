d62ccd73ed54bc2d074a09e6f29025214136d9fd
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-25 18:14:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let array = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(array, 'boshlanishdagi array');
+
+
+let juftSonlar = array.filter(son => son % 2 === 0);
+console.log(juftSonlar, 'juft sonlarga ajratilgan array');
+
+
